5121598d28cd795dcad7c420fc6bc542d326432f
Alfredo Ascanio <alspeed09@gmail.com>
2015-08-06 14:57:54 -0430

    Function to extract the authors from a BibTex file
    This function was create for extrac the authors from a BibTex file. This
    makes a matrix of characters in wich every element has the following
    format: "Surname, Name" . This can have many purposes, from review the
    different authors of the papers cited to make frecuency tables and other
    analyses.

---
+++ bibAuthors.R
@@ -0,0 +1,39 @@
+# --- 
+# title: "bibAuthors" 
+# author: "Alfredo Ascanio" 
+# date: "08/06/2015"
+# e-mail: "11-10060@usb.ve"
+# --- 
+
+bibAuthors <- function(x) {
+        
+        y <- x$author
+        a <- list()
+        b <- as.character(c())
+        
+        for(i in 1:length(y)) {
+        
+                for(j in 1:length(y[[i]])) {
+        b[j] <- c(paste(c(y[[i]][[j]]$family, y[[i]][[j]]$given), collapse=", "))
+        
+                }
+        b <- b[1:length(y[[i]])]
+        a[[i]] <- b
+        }
+        
+        vl <- NULL
+        for(i in 1:length(a)) {
+                vl[i] <- length(a[[i]])
+        }
+        e <- max(vl)
+        
+        for(i in 1:length(a)) {
+                length(a[[i]]) <- e
+        }
+        
+        z <- do.call("rbind", a)
+        
+        z
+        
+        
+}
